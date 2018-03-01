2a91ad5bdfb34221ec7eeb9775d85b6f4e8d5b84
salma-91 <salma.zegdane@gmail.com>
2018-03-01 14:24:51 +0100

    first commit

--- src/main/java/com/hbaseguide/chap03/PutExample.java
+++ src/main/java/com/hbaseguide/chap03/PutExample.java
@@ -2,12 +2,9 @@ package com.hbaseguide.chap03;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
