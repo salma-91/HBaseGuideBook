e59ef49da6db5ea16a562a5edd736bd21f1178bc
salma-91 <salma.zegdane@gmail.com>
2018-03-01 14:23:34 +0100

    first commit

--- src/main/java/com/hbaseguide/chap03/PutExample.java
+++ src/main/java/com/hbaseguide/chap03/PutExample.java
@@ -2,9 +2,12 @@ package com.hbaseguide.chap03;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -13,21 +16,26 @@ import java.io.IOException;
 
 public class PutExample {
 
-  public static void main(String[] args) throws IOException {
-    Configuration conf = HBaseConfiguration.create(); 
+	public static void main(String[] args) throws IOException {
+		Configuration conf = HBaseConfiguration.create();
+		/*
+		 * if not exist===> ERROR [main] (ConnectionManager.java:900) - The node /hbase
+		 * is not in ZooKeeper. It should have been written by the master. Check the
+		 * value configured in 'zookeeper.znode.parent'. There could be a mismatch with
+		 * the one configured in the master.
+		 */
+		conf.set("zookeeper.znode.parent", "/hbase-unsecure");
 
-    Connection connection = ConnectionFactory.createConnection(conf);
-    Table table = connection.getTable(TableName.valueOf("testtable")); 
+		Connection connection = ConnectionFactory.createConnection(conf);
 
-    Put put = new Put(Bytes.toBytes("row1")); 
+		Table table = connection.getTable(TableName.valueOf("testtable"));
+		Put put = new Put(Bytes.toBytes("row1"));
+		put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"));
+		put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("val2"));
 
-    put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"),
-      Bytes.toBytes("val1")); 
-    put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"),
-      Bytes.toBytes("val2")); 
+		table.put(put);
+		table.close();
+		connection.close();
 
-    table.put(put); 
-    table.close(); 
-    connection.close();
-  }
+	}
 }
